1ab372ac8148fa535937ef25d92847bc88ae5a97
Dayniel Lazaro <mdayniel@gmail.com>
2024-05-24 06:42:12 -0400

    many changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output:'hybrid'
+  output: 'server',
+  integrations: [auth()],
+  adapter: vercel()
 });
--- src/libs/products.js
+++
@@ -1,9 +0,0 @@
-import products from '../mock/products.json'
-
-export const getAllProduct = () => {
-    return products;
-}
-
-export const getProductByID = id => {
-    return products.filter(p => p.id == id)[0];
-}
